e11c64dafe12e001db43429e9638bb49e1b0aece
Josh Taylor <joshyt08@hotmail.co.uk>
2021-12-20 23:48:50 +0000

    Added Shape handler
    Added Shape handler to untilise shape factory and hide classes and methods behind the Shape process

---
+++ ShapeGrid/ShapeGridLibrary/ShapeHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace ShapeGridLibrary
+{
+    public class ShapeHandler
+    {
+        private readonly Shape _shape;
+
+        public ShapeHandler(ShapeType shapeType, int height, int width)
+        {
+            IShapeFactory shapeFactory = new ShapeFactory(shapeType, height, width);
+            _shape = new Shape(shapeFactory);
+        }
+
+        public string GetShapeLocation(int[][] coordinates)
+        {
+            return _shape.GetLocationFromCoordinates(coordinates);
+        }
+
+        public int[][] GetShapeCoordinates(string row, int column)
+        {
+            int rowNumericalValue = GetCharInt(row);
+            return _shape.GetCoordinatesFromLocation(rowNumericalValue, column);
+        }
+        
+        private int GetCharInt(string row)
+        {
+            char[] chars = row.ToUpper().ToCharArray();
+            return ((chars.Length - 1) * 26) + Convert.ToInt32(chars[^1] - 64);
+        }
+    }
+}
