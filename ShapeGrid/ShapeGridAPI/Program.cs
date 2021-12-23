4430ba9e093651e3f85e475a73fb0b5a323f046d
Josh Taylor <joshyt08@hotmail.co.uk>
2021-12-23 22:06:47 +0000

    Updated to allow for cors
    Updated to allow for cors with app created

--- ShapeGrid/ShapeGridAPI/Program.cs
+++ ShapeGrid/ShapeGridAPI/Program.cs
@@ -1,14 +1,24 @@
 using ShapeGridAPI;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:62561",
+                              "http://localhost:4200");
+                      });
+});
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +30,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
